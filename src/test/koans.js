9fa99556872e673c98db1b46b3a4f97315ffd954
Alejandro Gómez <alejandro@dialelo.com>
2014-07-26 22:30:22 +0200

    Add tests for proposed exercises

--- src/test/koans.js
+++ src/test/koans.js
@@ -30,3 +30,91 @@ describe('Tutorial examples', function(){
         expect(isOdd(3)).to.be.true;
     });
 });
+
+
+describe('Function composition', function(){
+    it.skip('Improve ~compose~ so the function it generates can take multiple arguments', function(){
+        var argsAsArr = (...args) => args;
+        var length = (xs) => xs.length;
+
+        betterCompose = () => null;
+
+        expect(betterCompose(length, argsAsArr)(1, 2)).to.be.equal(2);
+    });
+
+    it.skip('Write a variadic version of ~compose~', function(){
+        var add1 = (x) => x + 1;
+
+        composeAll = () => null;
+
+        expect(composeAll(add1, add1, add1)(0)).to.be.equal(3);
+    });
+});
+
+describe('Higher order functions', function(){
+    it.skip('Write a function that, given a number, returns a function that takes a number and adds it to the first', function(){
+        adder = () => null;
+
+        expect(adder(3)(4)).to.be.equal(7);
+    });
+
+    it.skip('Write the same function for the multiplication operation', function(){
+        multiplier = () => null;
+
+        expect(multiplier(3)(4)).to.be.equal(12);
+        expect(multiplier(2)(4)).to.be.equal(8);
+    });
+
+    it.skip('Write a function that, given a function, returns another function that calls to original one with all but the last argument', function(){
+        var argLength = (...args) => args.length;
+        var callWithAllButLast = () => null;
+
+        expect(callWithAllButLast(argLength)(1, 2, 3)).to.be.equal(2);
+    });
+});
+
+
+describe('Recursion', function(){
+    it.skip('Write a recursive function that, given a value and a list, returns a list with the first occurrence of the value removed', function() {
+        rember = () => null;
+
+        expect(rember(4, [1, 2, 3])).to.be.eql([1, 2, 3]);
+        expect(rember(1, [1, 2, 3])).to.be.eql([2, 3]);
+        expect(rember(2, [1, 2, 3])).to.be.eql([1, 3]);
+        expect(rember(3, [1, 2, 3])).to.be.eql([1, 2]);
+    });
+
+    it.skip('Write the same function in accumulator-passing style', function(){
+        rember = () => null;
+
+        expect(rember(4, [1, 2, 3])).to.be.eql([1, 2, 3]);
+        expect(rember(1, [1, 2, 3])).to.be.eql([2, 3]);
+        expect(rember(2, [1, 2, 3])).to.be.eql([1, 3]);
+        expect(rember(3, [1, 2, 3])).to.be.eql([1, 2]);
+
+    });
+});
+
+describe('Internal & External iteration', function(){
+    it.skip('Write your own version of ~map~, ~filter~ and ~reduce~ (you are allowed to use ~for~ loops and mutation)', function(){
+        map = () => null;
+        filter = () => null;
+        reduce = () => null;
+
+        expect(map((x) => x + 1, [0, 1, 2])).to.be.eql([1, 2, 3]);
+        expect(filter((x) => x % 2 === 0, [1, 2, 3, 4, 5])).to.be.eql([1, 3, 5]);
+        expect(reduce((x, y) => x + y, [1, 2, 3])).to.be.equal(6);
+    });
+});
+
+describe('Currying & Partial Application', function(){
+    it.skip('autoCurry', function() {
+        autoCurry = () => null;
+        curriedSum3 = autoCurry(function(x, y, z) { return x + y + z; });
+
+        expect(curriedSum3(1)(2)(3)).to.be.equal(6);
+        expect(curriedSum3(1)(2, 3)).to.be.equal(6);
+        expect(curriedSum3(1, 2, 3)).to.be.equal(6);
+        expect(curriedSum3()(1, 2)(3)).to.be.equal(6);
+    });
+});
